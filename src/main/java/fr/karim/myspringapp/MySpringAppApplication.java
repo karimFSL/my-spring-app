ca50c368180984c043867fa2e0a8c85bd986cd6b
karimFSL <salah.fisli@gmail.com>
2021-02-20 23:58:08 +0100

    troisieme test

--- src/main/java/fr/karim/myspringapp/MySpringAppApplication.java
+++ src/main/java/fr/karim/myspringapp/MySpringAppApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MySpringAppApplication {
 
 	
-	//test
+	//test2
 	public static void main(String[] args) {
 		SpringApplication.run(MySpringAppApplication.class, args);
 	}
