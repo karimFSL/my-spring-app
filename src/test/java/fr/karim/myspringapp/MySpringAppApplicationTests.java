2e91fab74a7b56b147796df1235adb067dca2ab7
karimFSL <salah.fisli@gmail.com>
2021-02-20 23:12:54 +0100

    first commit

---
+++ src/test/java/fr/karim/myspringapp/MySpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.karim.myspringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
