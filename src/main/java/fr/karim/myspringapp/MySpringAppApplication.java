75999d33b548062d599aa43f0ad9bb698fc2be15
karimFSL <salah.fisli@gmail.com>
2021-02-20 23:45:03 +0100

    second commit my feature

--- src/main/java/fr/karim/myspringapp/MySpringAppApplication.java
+++ src/main/java/fr/karim/myspringapp/MySpringAppApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MySpringAppApplication {
 
+	
+	//test
 	public static void main(String[] args) {
 		SpringApplication.run(MySpringAppApplication.class, args);
 	}
